1b0c255bcf65a87a70e73eccc88704e002e5a2f6
iboti57 <72953173+iboti57@users.noreply.github.com>
2021-08-06 16:38:15 +0200

    added

---
+++ server.js
@@ -0,0 +1,145 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var session = require("express-session");
+var mysql = require("mysql");
+// Stellt eine Verbindung zum Datenbankserver her
+var connection = mysql.createConnection({
+    database: "modul-server",
+    host: "localhost",
+    user: "root"
+});
+// Öffnet die Verbindung zum Datenbankserver
+connection.connect(function (err) {
+    if (err !== null) {
+        console.log("DB-Fehler: " + err);
+    }
+    else {
+        console.log("Database connected");
+    }
+});
+// Erzeugt und startet einen Express-Server
+var router = express();
+router.listen(8080, function () {
+    console.log("Server gestartet auf http://localhost:8080");
+});
+// Bei jedem Request werden vorhandene Nutzdaten von Text in Objekte geparst
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+// Bei jedem Request wird, falls noch nicht vorhanden, ein Cookie erstellt
+router.use(session({
+    cookie: {
+        expires: new Date(Date.now() + (1000 * 60 * 60)),
+    },
+    secret: Math.random().toString(),
+}));
+// Der Ordner ./view/res wird auf die URL /res gemapped
+router.use("/res", express.static(__dirname + "/view/res"));
+// Gibt auf der URL / die Startseite zurück
+router.get("/", function (req, res) {
+    res.sendFile(__dirname + "/view/index.html");
+});
+// Beschreibt alle Routen und ruft die jeweilige Funktion oder Funktionskette auf
+router.post("/signIn", signIn);
+router.post("/signOut", signOut);
+router.post("/task", checkLogin, addTask);
+router.delete("/task/:id", checkLogin, delTask);
+router.get("/tasks", checkLogin, getTasks);
+router.get("/isLoggedIn", checkLogin, isLoggedIn);
+// Prüft, ob ein Nutzer registriert ist und speichert ggf. den Nutzernamen im Sessionstore ab
+function signIn(req, res) {
+    var signInName = req.body.signInName;
+    var signInPass = req.body.signInPass;
+    query("SELECT * FROM users WHERE name = ? AND password = ?;", [signInName, signInPass]).then(function (result) {
+        if (result.length === 0) {
+            res.sendStatus(404);
+        }
+        else {
+            req.session.signInName = signInName;
+            res.sendStatus(200);
+        }
+    }).catch(function () {
+        // DBS-Fehler
+        res.sendStatus(500);
+    });
+}
+// Löscht den Sessionstore und weist den Client an, das Cookie zu löschen
+function signOut(req, res) {
+    req.session.destroy(function () {
+        res.clearCookie("connect.sid");
+        res.sendStatus(200);
+    });
+}
+// Fügt einen neuen Task der Datenbank hinzu
+function addTask(req, res) {
+    var taskName = req.body.taskName;
+    var taskDate = req.body.taskDate;
+    if (taskName !== undefined && taskDate !== undefined) {
+        query("INSERT INTO tasks (name,titel, faelligkeit) VALUES (?,?,?);", [req.session.signInName, taskName, taskDate]).then(function () {
+            console.log("added succesfully");
+            res.sendStatus(201);
+        }).catch(function (err) {
+            res.sendStatus(500);
+            console.log("Fehler in POST /module");
+            console.log(err);
+        });
+    }
+    else {
+        res.sendStatus(400);
+    }
+}
+// Löscht einen Task aus der Datenbank
+function delTask(req, res) {
+    var id = req.params.id;
+    query("DELETE FROM tasks WHERE id = ?;", [id]).then(function (result) {
+        if (result.affectedRows === 1) {
+            console.log("deleted succesfully");
+            res.sendStatus(200);
+        }
+        else {
+            res.sendStatus(404);
+        }
+        ;
+    }).catch(function () {
+        // DBS-Fehler
+        res.sendStatus(500);
+    });
+}
+// Gibt alle Tasks eines Anwenders zurück
+function getTasks(req, res) {
+    query("SELECT id, titel, faelligkeit FROM tasks WHERE name = ?;", [req.session.signInName]).then(function (result) {
+        res.json(result);
+    }).catch(function () {
+        // DBS-Fehler
+        res.sendStatus(500);
+    });
+}
+// Eine sog. Middleware-Route prüft, ob der Client angemeldet ist und ruft ggf. die nächste Funktion auf
+function checkLogin(req, res, next) {
+    if (req.session.signInName !== undefined) {
+        // Ruft die nächste Funktion der Funktioskette
+        next();
+    }
+    else {
+        // Client nicht angemeldet
+        res.sendStatus(401);
+    }
+}
+// Kleine Hilfsfunktion, die immer 200 OK zurückgibt
+function isLoggedIn(req, res) {
+    res.sendStatus(200);
+}
+// Ein eigener Wrapper, um die MySQL-Query als Promise (then/catch Syntax) zu nutzen
+function query(sql, param) {
+    if (param === void 0) { param = []; }
+    return new Promise(function (resolve, reject) {
+        connection.query(sql, param, function (err, results) {
+            if (err === null) {
+                resolve(results);
+            }
+            else {
+                reject(err);
+            }
+        });
+    });
+}
